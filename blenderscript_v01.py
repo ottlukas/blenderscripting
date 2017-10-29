ae259ddafe564fec13ef5f2b120140612b2d5926
ottlukas <ott.lukas.14@gmail.com>
2017-10-29 21:54:56 +0100

    added material creation
    one step closer to full automation ;)

--- blenderscript_v01.py
+++ blenderscript_v01.py
@@ -2,6 +2,9 @@ import bpy
 import bmesh
 
 def create():
+    scn = bpy.context.scene
+    scn.render.engine = 'CYCLES'
+    scn.world.use_nodes = True
     set_background()    
     #context
     bpyscene = bpy.context.scene
@@ -24,23 +27,26 @@ def create():
     # add subsurf mod and smooth
     bpy.ops.object.modifier_add(type='SUBSURF')
     bpy.ops.object.shade_smooth()
-
     # add material to the UV Sphere
     ob = bpy.context.active_object
+    ob.select = True
+    #scene.objects.active = ob
 
     # Get material
-    mat = bpy.data.materials.get("white_glow")
-    if mat is None:
+    g_mat = bpy.data.materials.get("green_glow")
+    if g_mat is None:
         # create material
-        mat = bpy.data.materials.new(name="white_glow")
-
+        create_materials("green_glow")        
+    g_mat = bpy.data.materials.get("green_glow")
     # Assign it to object
     if ob.data.materials:
         # assign to 1st material slot
-        ob.data.materials[0] = mat
+        ob.data.materials[0] = g_mat        
     else:
         # no slots
-        ob.data.materials.append(mat)
+        ob.data.materials.append(g_mat)
+        print ("Green Material assigned")
+    
     # move UV Sphere
     bpy.ops.object.move_to_layer(layers=(False, False, False, False, False, False, False, False, False, False, True, False, False, False, False, False, False, False, False, False))
 
@@ -61,14 +67,16 @@ def create():
         bpy.ops.font.text_insert(text=char)
     bpy.ops.object.editmode_toggle()
     # add material to the Text
+    ob.select = True
     ob = bpy.context.active_object
+    #scene.objects.active = ob
 
     # Get material
-    mat = bpy.data.materials.get("glow")
+    mat = bpy.data.materials.get("white_glow")
     if mat is None:
         # create material
-        mat = bpy.data.materials.new(name="glow")
-
+        create_materials("white_glow")
+    mat = bpy.data.materials.get("white_glow")
     # Assign it to object
     if ob.data.materials:
         # assign to 1st material slot
@@ -76,7 +84,7 @@ def create():
     else:
         # no slots
         ob.data.materials.append(mat)
-
+    print ("Green Material assigned")
     # text to mesh
     bpy.ops.object.convert(target='MESH')
     # Particle system
@@ -89,6 +97,39 @@ def set_background():
     # darken the background    
     bpy.context.scene.world.node_tree.nodes['Background'].inputs['Color'].default_value = (0, 0, 0, 1)
     print("Set background color")
+
+def create_materials(mat_name):
+    mat = bpy.data.materials.new(name=mat_name)
+    # get the material
+    mat = bpy.data.materials[mat_name]
+    mat.use_nodes = True
+    # get the nodes
+    nodes = mat.node_tree.nodes
+    
+    # clear all nodes to start clean
+    for node in nodes:
+        nodes.remove(node)
+
+    # create emission node
+    node_emission = nodes.new(type='ShaderNodeEmission')
+    if (mat_name == "white_glow"):
+        node_emission.inputs[0].default_value = (1,1,1,1)  # white RGBA
+    else:
+        node_emission.inputs[0].default_value = (0,1,0,1)  # white RGBA
+    node_emission.inputs[1].default_value = 2.0 # strength
+    node_emission.location = 0,0
+
+    # create output node
+    node_output = nodes.new(type='ShaderNodeOutputMaterial')   
+    node_output.location = 400,0
+    
+    # link nodes
+    links = mat.node_tree.links
+    link = links.new(node_emission.outputs[0], node_output.inputs[0])
+
+    # remove links
+    #links.remove(link)
+
 
 # Animate Particle System
 #def animate():
